76a0884f6062b0da1cf324d69a4c98f2fcbc6036
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-04-21 06:54:46 +0300

    Popup widget

---
+++ PA-WorkShop-Web-Components/02. popup-widget/app.js
@@ -0,0 +1,16 @@
+const template = document.getElementById('popup-template');
+class InfoPopup extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+    connectedCallback() {
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        const imgSrc = this.getAttribute('img');
+        if (imgSrc) {
+            this.shadowRoot.querySelector('img').src = imgSrc;
+        }
+    }
+}
+
+customElements.define('info-popup', InfoPopup);
